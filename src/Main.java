63c5caf368ecf2aa51ce50e98fa4ef7d5d892eba
amogh <amoghreddykb1@gmail.com>
2025-09-05 23:38:27 +0530

    05/09-23:38

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 class Main{
 
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    int num;
+
+    public Main(int num) {
+        this.num = num;
     }
 
 
-    }
+
+}
--- src/OOPs/PolymorphismDemo.java
+++ src/OOPs/PolymorphismDemo.java
@@ -1,93 +1,116 @@
 package OOPs;
 
-// Polymorphism Demo Program
-class Animal {
-    // Method Overloading (Compile-time Polymorphism)
-    void sound() {
-        System.out.println("Animal makes a sound");
+// Base class
+class Payment {
+    // ---------- Method Overloading (Compile-time Polymorphism) ----------
+    void pay(double amount) {
+        System.out.println("Paying " + amount + " using default method");
     }
 
-    void sound(String mood) {
-        System.out.println("Animal makes a sound when it is " + mood);
+    void pay(double amount, String currency) {
+        System.out.println("Paying " + amount + " in " + currency);
     }
 
-    // Method to be overridden (Runtime Polymorphism)
-    void eat() {
-        System.out.println("Animal eats food");
-    }
-}
-
-// Subclass Dog extending Animal
-class Dog extends Animal {
-    // Overriding eat() (Runtime Polymorphism)
-    @Override
-    void eat() {
-        System.out.println("Dog eats bones");
+    // ---------- Method to be overridden ----------
+    void process() {
+        System.out.println("Processing generic payment");
     }
 
-    // Covariant return type example
-    @Override
-    Dog reproduce() {
-        System.out.println("Dog reproduces - returns Dog object");
+    // Method for covariant return type demo
+    Payment receipt() {
+        System.out.println("Generic receipt generated");
         return this;
     }
 }
 
-// Subclass Cat extending Animal
-class Cat extends Animal {
+// Subclass for Credit Card Payment
+class CreditCardPayment extends Payment {
     @Override
-    void eat() {
-        System.out.println("Cat eats fish");
+    void process() {
+        System.out.println("Processing payment via Credit Card");
     }
 
     @Override
-    Cat reproduce() {
-        System.out.println("Cat reproduces - returns Cat object");
+    CreditCardPayment receipt() {  // Covariant return type
+        System.out.println("Credit Card receipt generated");
+        return this;
+    }
+}
+
+// Subclass for UPI Payment
+class UpiPayment extends Payment {
+    @Override
+    void process() {
+        System.out.println("Processing payment via UPI");
+    }
+
+    @Override
+    UpiPayment receipt() {  // Covariant return type
+        System.out.println("UPI receipt generated");
         return this;
     }
 }
 
 // Interface example
-interface Pet {
-    void play();
+interface Refundable {
+    void refund(double amount);
 }
 
-// Dog implements Pet interface
-class PetDog extends Dog implements Pet {
+// Class implementing interface
+class WalletPayment extends Payment implements Refundable {
     @Override
-    public void play() {
-        System.out.println("Pet Dog plays with owner");
+    void process() {
+        System.out.println("Processing payment via Wallet");
+    }
+
+    @Override
+    public void refund(double amount) {
+        System.out.println("Refunding " + amount + " back to Wallet");
+    }
+
+    @Override
+    WalletPayment receipt() {
+        System.out.println("Wallet receipt generated");
+        return this;
+    }
+}
+
+// Demonstrating super and this usage
+class NetBankingPayment extends Payment {
+    @Override
+    void process() {
+        super.process(); // call parent method
+        System.out.println("Processing payment via Net Banking (extra security checks)");
     }
 }
 
 public class PolymorphismDemo {
     public static void main(String[] args) {
         // ---------- Compile-time Polymorphism ----------
-        Animal a1 = new Animal();
-        a1.sound(); // Calls sound()
-        a1.sound("angry"); // Calls overloaded sound(String)
+        Payment basePay = new Payment();
+        basePay.pay(1000);              // Overloaded method 1
+        basePay.pay(500, "USD");        // Overloaded method 2
 
-        // ---------- Runtime Polymorphism ----------
-        Animal a2 = new Dog(); // Upcasting (Animal reference, Dog object)
-        a2.eat(); // Calls Dog's eat() due to dynamic method dispatch
+        // ---------- Runtime Polymorphism (Dynamic Method Dispatch) ----------
+        Payment p1 = new CreditCardPayment(); // Upcasting
+        p1.process(); // Calls CreditCardPayment's process()
 
-        Animal a3 = new Cat(); // Upcasting
-        a3.eat(); // Calls Cat's eat()
+        Payment p2 = new UpiPayment();  // Upcasting
+        p2.process(); // Calls UpiPayment's process()
 
         // ---------- Covariant return types ----------
-        Dog d = new Dog();
-        d.reproduce(); // returns Dog
+        CreditCardPayment cc = new CreditCardPayment();
+        cc.receipt(); // returns CreditCardPayment
 
-        Cat c = new Cat();
-        c.reproduce(); // returns Cat
+        UpiPayment upi = new UpiPayment();
+        upi.receipt(); // returns UpiPayment
 
         // ---------- Interface Polymorphism ----------
-        Pet myPet = new PetDog(); // Pet reference, PetDog object
-        myPet.play(); // Calls PetDog's play()
+        Refundable wallet = new WalletPayment(); // Interface reference
+        wallet.refund(300); // Calls WalletPayment's refund()
 
-        // ---------- Using super and this ----------
-        // In real polymorphism, 'super' is used to call parent methods inside child
-        // 'this' refers to current object
-        System.out.println("\n--- super & this explained inside classes ---");
+        // ---------- super and this ----------
+        Payment nb = new NetBankingPayment();
+        nb.process(); // Calls super.process() + subclass process()
     }
 }
